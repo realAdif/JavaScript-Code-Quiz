7136e5215698e63b6e21dbe7a8b62672503a8f3a
Aditya <adityagaikwad693@gmail.com>
2022-06-08 23:48:13 +1000

    Update to Javascript Added Qustions and the options

--- script.js
+++ script.js
@@ -1,88 +1,153 @@
 var startButton = document.querySelector('.start-button');
 var timerEl = document.querySelector('#timer');
 var startPageEl = document.querySelector('#start');
-var QustionPageEl = document.querySelector("#qustionsOptions");
-var qustionsEl = document.querySelector("#qustion");
+var questionPageEl = document.querySelector("#questionsOptions");
+var questionsEl = document.querySelector("#question");
+var timeAddedEl = document.querySelector("#timerAdded");
+var highscoreEl = document.querySelector("#score");
+var optionEl = document.querySelector("#options");
+var highscorePageEl = document.querySelector("#Highscore-page");
+var timerLeftScoreEl = document.querySelector("#time-left-score");
 
-var optionOne = document.querySelector(".optionOne");
-var optionTwo = document.querySelector(".optionTwo");
-var optionThree = document.querySelector(".optionThree");
-var optionFour = document.querySelector(".optionFour");
+console.log(timerLeftScoreEl);
+var questionsArrays = [
+    {
+        title: "Arrays in JavaScript can be used to store:",
+        options: ['String','Boolen','Numbers', 'All of Above'],
+        answer: 3
+    },
+    {
+        title: "How do you wirte a Function:",
+        options: ['Function()','function nameFuncrion','functions', 'All of Above'],
+        answer: 1
+    },
+    {
+        title: "What is Boonlen?",
+        options: ['String','Numbers','True or False', 'All of Above'],
+        answer: 2
+    },
+    {
+        title: "What is a String?",
+        options: ['String','Boolen','Numbers', 'All of Above'],
+        answer: 0
+    }
+];
 
+highscorePageEl.style.display ='none';
+var highscoreObject = {
+    name: "Name",
+    score: 1,
+    time: 0
+};
+var scoreNumber = highscoreObject.score;
+var timerLeftScore = highscoreObject.time; 
+
+// the start up of the page:
 var secondsLeft = 20;
 var startPageMode = true;
 pageMode();
+onClick();
+highscorePagesNumber();
+
+// on click event 
+function onClick(){
+    startButton.addEventListener("click", function(){
+        startPageMode = false;
+        init();
+           
+    });
+}
+
+// All the Questions
+
+var currentIndex  = 0;
+
+
+function showQuestion(){   
+    questionsEl.textContent = questionsArrays[currentIndex].title;
+    questionsArrays[currentIndex].options;
+    
+    // console.log(currentOptions);
+  optionEl.innerHTML = "";
+    questionsArrays[currentIndex].options.forEach(function(option, index){
+        var buttonEl = document.createElement("button");
+        buttonEl.setAttribute('class','options');
+        buttonEl.setAttribute('id', index);
+        buttonEl.textContent = option;
+        optionEl.append(buttonEl);      
+    })
+    
+
+}
+
+
+timeAddedEl.innerHTML = "";
+optionEl.addEventListener('click', function(event){
+    // console.log(event.target.id);
+    var buttonIdex = event.target.id;
+    if(currentIndex < 3){
+        if(buttonIdex == questionsArrays[currentIndex].answer){
+            secondsLeft = secondsLeft + 10;
+            timeAddedEl.innerHTML = "+10";
+            scoreNumber++
+            console.log(scoreNumber);
+            timeAddedTimer();
+            currentIndex++;
+            showQuestion();
+            
+        }else{
+            secondsLeft = secondsLeft - 5;
+            timeAddedEl.innerHTML = "-5";
+            timeAddedTimer();
+        }
+    }else{
+        highscorePageEl.style.display ='block';
+        questionPageEl.style.display = 'none';
+        timerLeftScore = secondsLeft;
+        console.log(timerLeftScore);
+        highscorePagesNumber();
+    }   
+});
 
 
 
+//console.log(highscoreEl.innerHTML = " Score Number: "+ scoreNumber)
+//console.log( timerLeftScoreEl.innerHTML = "Time: "; +timerLeftScore)
+function highscorePagesNumber(){
+    highscoreEl.innerHTML = " Score Number: "+ scoreNumber;
+    timerLeftScoreEl.innerHTML = "Time: " +timerLeftScore;
+}
 
+function timeAddedTimer(){  
+    setInterval(function(){
+        timeAddedEl.innerHTML = "";
+    },500)  
+}
 
-startButton.addEventListener("click", function(){
-    startPageMode = false;
-    pageMode();
-    qustions();
-
+// timer function
+function timer(){
     var timerLeft = setInterval(function(){
         secondsLeft--;
         timerEl.innerHTML = secondsLeft
 
     },1000)
-
-
-})
-
-function qustions(){
-    var qustionsOne = 'Arrays in JavaScript can be used to store';
-    var qustionsTwo = 'How do you wirte a Function';
-    var qustionsThree = 'What is Boonlen?';
-    var qustionsFour = 'What is a String?';
-
-    var allQustions = [qustionsOne,qustionsTwo,qustionsThree,qustionsFour];
-
-    var randomQustions = allQustions[Math.floor(Math.random()* 4)];
-
-    console.log(randomQustions);
-    qustionsEl.textContent = randomQustions;
-    options()
-    
-
-    function options(){
-        if(randomQustions === allQustions[0]){
-            optionOne.textContent = 'Strings';
-            optionTwo.textContent = 'Numbers';
-            optionThree.textContent = 'Boolen';
-            optionFour.textContent = 'All of the Above';
-
-        }else if(randomQustions === allQustions[1]){
-            optionOne.textContent = 'MyFunction =';
-            optionTwo.textContent = 'Function';
-            optionThree.textContent = 'Function FuctionName(){}';
-            optionFour.textContent = 'none of the Above';
-
-        }else if(randomQustions === allQustions[2]){
-            optionOne.textContent = 'Strings';
-            optionTwo.textContent = 'Numbers';
-            optionThree.textContent = 'True or False';
-            optionFour.textContent = 'All of the Above';
-
-        }else if(randomQustions === allQustions[3]){
-            optionOne.textContent = 'Words';
-            optionTwo.textContent = 'Numbers';
-            optionThree.textContent = 'Boolen';
-            optionFour.textContent = 'All of the Above';
-
-        }
-    }
 }
 
 
+// page mode function
 function pageMode(){
 
-    if(startPageMode === true){
-        QustionPageEl.style.display = "none";
+    if(startPageMode === true){             //start Page = true
+        questionPageEl.style.display = "none";
         startPageEl.style.display = "block";
-    }else if(startPageMode === false){
-        QustionPageEl.style.display = "block";     
+    }else if(startPageMode === false){      // Question Page = false
+        questionPageEl.style.display = "block";     
         startPageEl.style.display = "none";
     }
+}
+
+function init(){
+    pageMode();
+    timer();
+    showQuestion();   
 }
